b3a31241dccf2186e2ddc0ef4226a6c80d08958a
Mateus Nascimento <mateus.nascimento@corp.globo.com>
2020-03-21 02:07:15 -0300

    [tensorflow] finally understanding newaxis trick

--- tensorflow/broadcasting_trick.py
+++ tensorflow/broadcasting_trick.py
@@ -1,23 +1,26 @@
 import numpy as np
 from time import time
 
-x = np.random.rand(10000,10000)
-y = np.random.rand(10000,10000)
+x = np.random.rand(100)
+W = np.random.rand(100,500)
 # print(x)
 
 x_trick = x[..., np.newaxis]
-y_trick = y[..., np.newaxis]
 # print(x_trick)
 
 start = time()
-ans = x * y
+ans = np.matmul(x, W)
 end = time()
 print(end - start)
 
 start = time()
-ans_trick = x_trick * y_trick
+ans_trick = np.matmul(W.T, x_trick)
 end = time()
 print(end - start)
 
-ans_trick = ans_trick[:,:,0]
-print(ans_trick == ans)
+ans_trick  = ans_trick[:,0]
+
+comparison = ans == ans_trick
+equal_arrays = comparison.all()
+
+print(equal_arrays)
