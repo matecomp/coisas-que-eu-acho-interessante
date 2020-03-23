8054a6ad02d31ce4799bc0a01779194d843badfd
Mateus Nascimento <mateus.nascimento@corp.globo.com>
2020-03-22 21:46:12 -0300

    [tensorflow] refactoring

--- tensorflow/linear-regression/web.js
+++ tensorflow/linear-regression/web.js
@@ -3,40 +3,6 @@ const HEIGHT = 650
 const RADIUS = 10
 
 const points = []
-const params = [1, 0]
-
-function setParams () {
-  for (let i = 0; i < model.getWeights().length; i++) {
-    params[i] = model.getWeights()[i].dataSync()[0]
-  }
-}
-
-let training = false
-function trainModel () {
-  if (points.length === 0 || training) {
-    return
-  }
-
-  training = true
-  tf.tidy(() => {
-    const data = tf.tensor1d(points.map(point => point.x / WIDTH))
-    const output = tf.tensor1d(points.map(point => (HEIGHT - point.y) / HEIGHT))
-    model
-      .fit(data, output, {
-        epochs: 1,
-        batchSize: 1
-      })
-      .then(info => {
-        setParams()
-        training = false
-      })
-  })
-}
-
-function predict (x) {
-  y = x * params[0] + params[1] * HEIGHT
-  return HEIGHT - y
-}
 
 const model = tf.sequential({
   layers: [
@@ -49,47 +15,77 @@ const model = tf.sequential({
 })
 
 model.compile({
-  optimizer: tf.train.sgd(
-    (learningRate = 0.01),
-    (momentum = 3),
-    (nesterov = true)
-  ),
+  optimizer: tf.train.sgd(0.01),
   loss: 'meanSquaredError'
 })
 
-setParams()
-setInterval(trainModel, 10)
+let training = false
+function trainModel () {
+  if (points.length === 0 || training) {
+    return
+  }
+
+  training = true
+  const data = tf.tensor1d(points.map(point => point.x))
+  const output = tf.tensor1d(points.map(point => point.y))
+
+  return model
+    .fit(data, output, {
+      epochs: 1,
+      batchSize: 1
+    })
+    .then(() => {
+      training = false
+    })
+}
+
+function predict (xs) {
+  tf_x = tf.tensor1d(xs)
+  return model.predict(tf_x)
+}
 
 function setup () {
   createCanvas(WIDTH, HEIGHT)
-  frameRate(60)
 }
 
 let borderLen = 0
 function mouseClicked () {
   points.push({
-    x: mouseX,
-    y: mouseY
+    x: map(mouseX, 0, WIDTH, 0, 1),
+    y: map(mouseY, 0, HEIGHT, 1, 0)
   })
   borderLen = RADIUS * 2
 }
 
 function draw () {
   background(0, 50, 0)
+  const xs_points = points.map(point => map(point.x, 0, 1, 0, WIDTH))
+  const ys_points = points.map(point => map(point.y, 0, 1, HEIGHT, 0))
 
   fill(150)
-  for (let idx in points) {
-    circle(points[idx].x, points[idx].y, RADIUS)
-  }
+  for (let idx = 0; idx < points.length; idx++) {
+    circle(xs_points[idx], ys_points[idx], RADIUS)
 
-  if (points.length > 0 && borderLen > 0) {
-    fill(255)
-    let idx = points.length - 1
-    circle(points[idx].x, points[idx].y, RADIUS + borderLen)
-    borderLen -= 1
+    if (idx === points.length - 1 && borderLen > 0) {
+      fill(255)
+      circle(xs_points[idx], ys_points[idx], RADIUS + borderLen)
+      borderLen -= 1
+    }
   }
 
   stroke(255)
-  line(0, predict(0), WIDTH, predict(WIDTH))
+  tf.tidy(() => {
+    trainModel()
+
+    let xs = [0, 1]
+    let x1 = map(xs[0], 0, 1, 0, WIDTH)
+    let x2 = map(xs[1], 0, 1, 0, WIDTH)
+
+    ys = predict(xs).dataSync()
+    let y1 = map(ys[0], 0, 1, HEIGHT, 0)
+    let y2 = map(ys[1], 0, 1, HEIGHT, 0)
+
+    line(x1, y1, x2, y2)
+  })
   stroke(0)
 }
